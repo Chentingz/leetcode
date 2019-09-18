44344b04619f6e0cef44feab6c8dc561b0c575e5
Chentingz <ct2ing@gmail.com>
2019-09-18 19:18:50 +0800

    #21 AC on 20190918

--- 21.merge-two-sorted-lists.cpp
+++ 21.merge-two-sorted-lists.cpp
@@ -34,45 +34,50 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1 && !l2)  return NULL;
+        if(l1 && !l2)   return l1;
+        if(!l1 && l2)   return l2;
+
         ListNode* l1Tmp=l1,*l2Tmp=l2;
-        ListNode* l3=NULL,*l3Tmp=l3;
+        ListNode* l3=NULL, *l3Tmp = l3;
         
         // 确定首节点
         if(l1Tmp->val >= l2Tmp->val){
-            *l3 = ListNode(l2Tmp->val);
+            l3 = l2Tmp;
             l2Tmp = l2Tmp->next;
         }
         else{
-            *l3 = ListNode(l1Tmp->val);
+            l3 = l1Tmp;
             l1Tmp = l1Tmp->next;
         }
         l3Tmp = l3;
         while(l1Tmp && l2Tmp){
             if(l1Tmp->val >= l2Tmp->val){
-                *l3Tmp->next = ListNode(l2Tmp->val);
+                l3Tmp->next = l2Tmp;
                 l3Tmp = l3Tmp->next;
                 l2Tmp = l2Tmp->next;
             }
             else if(l1Tmp->val < l2Tmp->val){
-                *l3Tmp->next = ListNode(l1Tmp->val);
+                l3Tmp->next = l1Tmp;
                 l3Tmp = l3Tmp->next;
                 l1Tmp = l1Tmp->next;
             }
         }
         if(l1Tmp == NULL)
-            // l3Tmp->next = l2Tmp;
-            while(l2Tmp){
-                *l3Tmp->next = ListNode(l2Tmp->val);
-                l3Tmp = l3Tmp->next;
-                l2Tmp = l2Tmp->next;
-            }
+            l3Tmp->next = l2Tmp;
+            // while(l2Tmp){
+            //     *l3Tmp->next = ListNode(l2Tmp->val);
+            //     l3Tmp = l3Tmp->next;
+            //     l2Tmp = l2Tmp->next;
+            // }
         if(l2Tmp == NULL)
-            // l3Tmp->next = l1Tmp;
-            while(l1Tmp){
-                *l3Tmp->next = ListNode(l1Tmp->val);
-                l3Tmp = l3Tmp->next;
-                l1Tmp = l1Tmp->next;
-            }
+            l3Tmp->next = l1Tmp;
+            // while(l1Tmp){
+            //     *l3Tmp->next = ListNode(l1Tmp->val);
+            //     l3Tmp = l3Tmp->next;
+            //     l1Tmp = l1Tmp->next;
+            // }
+
         return l3;
     }
 };
